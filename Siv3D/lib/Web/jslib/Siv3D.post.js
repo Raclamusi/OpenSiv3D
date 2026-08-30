a59f0ca15368eafd7418ee7c93fd55b889ad190e
Raclamusi <raclamusi@gmail.com>
2026-08-30 22:19:47 +0900

    [Web] キャンバスの外でマウスボタンを離したことを検出 #28

--- Siv3D/lib/Web/jslib/Siv3D.post.js
+++ Siv3D/lib/Web/jslib/Siv3D.post.js
@@ -53,6 +53,45 @@
             }
         });
     }
+
+    // Workaround for mouseup event not firing when the mouse is released outside the canvas
+    {
+        let mouseButtonState = 0;
+        /** @param {MouseEvent} event */
+        const updateMouseButtonState = event => {
+            if (!event.isTrusted || event.button < 0 || event.button > 2) {
+                return;
+            }
+            {
+                const buttonBit = [1, 4, 2][event.button];
+                if (event.type === "mousedown") {
+                    mouseButtonState |= buttonBit;
+                }
+                else if (event.type === "mouseup") {
+                    mouseButtonState &= ~buttonBit;
+                }
+            }
+            const unreleasedButtons = mouseButtonState & ~event.buttons;
+            for (let button = 0; button <= 2; ++button) {
+                const buttonBit = [1, 4, 2][button];
+                if (unreleasedButtons & buttonBit) {
+                    mouseButtonState &= ~buttonBit;
+                    event.target.dispatchEvent(new MouseEvent("mouseup", {
+                        button: button,
+                        buttons: mouseButtonState,
+                        clientX: event.clientX,
+                        clientY: event.clientY,
+                        screenX: event.screenX,
+                        screenY: event.screenY,
+                    }));
+                }
+            }
+        };
+        const canvas = Module["canvas"];
+        canvas.addEventListener("mousedown", updateMouseButtonState);
+        canvas.addEventListener("mousemove", updateMouseButtonState);
+        canvas.addEventListener("mouseup", updateMouseButtonState);
+    }
 })();
 
 __ATEXIT__.push(function() {
